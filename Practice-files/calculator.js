70566ccec998dd995f9b13de94ae8e53fec0e09b
Sayanta337 <sayantabhattacharya592@gmail.com>
2025-08-08 12:23:53 +0530

    update-008 calculator done. finishing touch pending.

--- Practice-files/calculator.js
+++ Practice-files/calculator.js
@@ -1,1 +1,71 @@
 //const dev = "Sayanta"
+let answer = '';
+
+function add1() {
+  answer = answer + '1';
+  document.querySelector('.display').value = answer;
+}
+function add2() {
+  answer = answer + '2';
+  document.querySelector('.display').value = answer;
+}
+function add3() {
+  answer = answer + '3';
+  document.querySelector('.display').value = answer;
+}
+function add4() {
+  answer = answer + '4';
+  document.querySelector('.display').value = answer;
+}
+function add5() {
+  answer = answer + '5';
+  document.querySelector('.display').value = answer;
+}
+function add6() {
+  answer = answer + '6';
+  document.querySelector('.display').value = answer;
+}
+function add7() {
+  answer = answer + '7';
+  document.querySelector('.display').value = answer;
+}
+function add8() {
+  answer = answer + '8';
+  document.querySelector('.display').value = answer;
+}
+function add9() {
+  answer = answer + '9';
+  document.querySelector('.display').value = answer;
+}
+function add0() {
+  answer = answer + '0';
+  document.querySelector('.display').value = answer;
+}
+function add00() {
+  answer = answer + '00';
+  document.querySelector('.display').value = answer;
+}
+function adddot() {
+  answer = answer + '.';
+  document.querySelector('.display').value = answer;
+}
+function plus() {
+  answer = answer + '+';
+  document.querySelector('.display').value = answer;
+}
+function minus() {
+  answer = answer + '-';
+  document.querySelector('.display').value = answer;
+}
+function multiply() {
+  answer = answer + '*';
+  document.querySelector('.display').value = answer;
+}
+function divide() {
+  answer = answer + '/';
+  document.querySelector('.display').value = answer;
+}
+function result() {
+  answer = eval(answer);
+  document.querySelector('.display').value = answer;
+}
