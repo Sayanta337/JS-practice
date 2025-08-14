ff92ac5babc99fea0c72af4f55278ce1e061cc44
Sayanta337 <sayantabhattacharya592@gmail.com>
2025-08-14 20:11:33 +0530

    update-015

--- rock-paper-scissor-game/game.js
+++ rock-paper-scissor-game/game.js
@@ -1,2 +1,10 @@
-console.log("Developer - Sayanta")
+console.log("The developer of this site is Sayanta")
 let bulbBG = document.querySelector('.cls3')
+let winAlert = document.querySelector('.display')
+let gameInfo = document.querySelector('.optionOutput')
+let rockbtn = document.querySelector('#rock')
+let paperbtn = document.querySelector('#paper')
+let scissorbtn = document.querySelector('#scissor')
+
+winAlert.innerText = "you won"
+gameInfo.innerText = "you chose rock"
