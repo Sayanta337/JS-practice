10dbec74bf5bba4315b25f73cfb721ba376ef706
Sayanta337 <sayantabhattacharya592@gmail.com>
2025-08-17 20:26:04 +0530

    update-018 now border color changes depending on the game info.

--- rock-paper-scissor-game/game.js
+++ rock-paper-scissor-game/game.js
@@ -2,15 +2,14 @@ console.log("The developer of this site is Sayanta (you can call me Leo)");
 let bulbBG = document.querySelector('#thebulb');
 const winAlert = document.querySelector('.display');
 const gameInfo = document.querySelector('.optionOutput');
-const rockbtn = document.querySelector('#rock');
-const paperbtn = document.querySelector('#paper');
-const scissorbtn = document.querySelector('#scissor');
+let bColor = document.querySelector('.total');
 let xNumber;
 let computerChoice;
 let userChoice;
 bulbBG.setAttribute("fill", "#f0f8ff");
-gameInfo.innerText = "press a button"
-winAlert.innerText = "welcome!!"
+bColor.style.borderColor = "#00324b";
+gameInfo.innerText = "press a button";
+winAlert.innerText = "welcome!!";
 
 //computer choice validator
 function ccgen() {
@@ -21,8 +20,8 @@ if (0 < xNumber && 2 > xNumber) {
   computerChoice = 'paper'
 } else if (4 < xNumber) {
   computerChoice = 'scissor'
-};
 }
+};
 
 function finalRclick() {
   userChoice = 'rock';
@@ -47,29 +46,36 @@ function winMsg() {
 if (computerChoice == userChoice){
   winAlert.innerText = "it's a tie!!";
   bulbBG.setAttribute("fill", "#00c8ff");
+  bColor.style.borderColor = "#00c8ff";
 }
 else if (userChoice == 'rock' && computerChoice =='paper'){
 winAlert.innerText = "computer won!!";
 bulbBG.setAttribute("fill", "#f0794a");
+bColor.style.borderColor = "#f0794a";
 }
 else if (userChoice == 'rock' && computerChoice =='scissor'){
 winAlert.innerText = "user won!!";
 bulbBG.setAttribute("fill", "#f0d246");
+bColor.style.borderColor = "#f0d246";
 }
 else if (userChoice == 'paper' && computerChoice =='rock'){
 winAlert.innerText = "user won!!";
 bulbBG.setAttribute("fill", "#f0d246");
+bColor.style.borderColor = "#f0d246";
 }
 else if (userChoice == 'paper' && computerChoice =='scissor'){
 winAlert.innerText = "computer won!!";
 bulbBG.setAttribute("fill", "#f0794a");
+bColor.style.borderColor = "#f0794a";
 }
 else if (userChoice == 'scissor' && computerChoice =='rock'){
 winAlert.innerText = "computer won!!";
 bulbBG.setAttribute("fill", "#f0794a");
+bColor.style.borderColor = "#f0794a";
 }
 else if (userChoice == 'scissor' && computerChoice =='paper'){
 winAlert.innerText = "user won!!";
 bulbBG.setAttribute("fill", "#f0d246");
+bColor.style.borderColor = "#f0d246";
 }
 };
