575ae8e0be4f867a52271b10ffe149e824d77ef7
Sayanta337 <sayantabhattacharya592@gmail.com>
2025-09-20 11:26:14 +0530

    update-031 notes app js progressed

--- Notes/notes.js
+++ Notes/notes.js
@@ -1,1 +1,16 @@
 console.log("The developer of this site is Sayanta (you can call me Leo)");
+//input section
+let uInput = document.querySelector("#user_input");
+let addBtn = document.querySelector("#input_submit");
+let inArray = [];
+// output section
+let uOutput = document.querySelector(".para_notes");
+let trashBtn = document.querySelector(".delete_btn");
+
+function addValue(){
+  if (uInput.value){
+    inArray.push(uInput.value);
+    console.log(inArray);
+  }
+  else {uInput.placeholder = "Please type something first..."};
+};
